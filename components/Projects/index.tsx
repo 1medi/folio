cbb22e3da4cfe0626578ad692427b2a4338e6856
1medi <144403286+1medi@users.noreply.github.com>
2025-01-13 10:50:39 -0800

    ok

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -13,7 +13,8 @@ const projects = [
     description: "Description for Project 1",
     image: Aether,
     backgroundColor: "#0abaef",
-    pathName:"/Aether"
+    transform: "translateX(-1000px)",
+    pathName: "/Aether",
   },
   {
     id: 2,
@@ -21,7 +22,8 @@ const projects = [
     description: "Description for Project 2",
     image: Oscar,
     backgroundColor: "#656384",
-    pathName:"/Pocket-Prof"
+    transform: "translateX(1000px)",
+    pathName: "/Pocket-Prof",
   },
   {
     id: 3,
@@ -29,7 +31,8 @@ const projects = [
     description: "Description for Project 3",
     image: LB,
     backgroundColor: "#f0ab1a",
-    pathName:"/Lutong-Bahay"
+    transform: "translateX(1000px)",
+    pathName: "/Lutong-Bahay",
   },
   {
     id: 4,
@@ -37,7 +40,8 @@ const projects = [
     description: "Description for Project 4",
     image: ACG,
     backgroundColor: "#DD8143",
-    pathName:"/ACG"
+    transform: "translateX(1000px)",
+    pathName: "/ACG",
   },
 ];
 
@@ -48,31 +52,39 @@ const Projects = () => {
     description: string;
     image: string;
     backgroundColor: string;
+    transform: string;
     pathName: string;
   } | null>(null);
 
   return (
     <>
       <div className="flex flex-col items-center gap-14 mt-4">
-        <div className="flex flex-col gap-14 mt-8">
-          <h1 className="text-7xl text-center">Projects</h1>
+        <div className=" gap-14 mt-8">
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <h1 className="text-7xl text-center">Projects</h1>
+          </motion.div>
+
           {projects.map((project) => (
             <motion.div
               key={project.id}
-              className="p-14 h-4 gap-12 flex flex-row border rounded cursor-pointer"
-              style={{ margin: 14, padding: 100, backgroundColor: project.backgroundColor}}
+              className="p-20 m-12 h-full flex-row border rounded cursor-pointer"
+              style={{ backgroundColor: project.backgroundColor }}
               onClick={() => setSelectedProject(project)}
+              initial={{ transform: project.transform }}
+              animate={{ transform: "translateX(0px)" }}
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
               <Image
+                className="m-auto"
                 src={project.image}
                 alt={project.title}
-                width={0}
-                height={100}
-                className="w-full h-auto"
+                height={150}
+                width={150}
               />
-              <h2 className="text-xl font-semibold">{project.title}</h2>
+              <h2 className="text-2xl text-center font-semibold">
+                {project.title}
+              </h2>
             </motion.div>
           ))}
         </div>
@@ -95,13 +107,16 @@ const Projects = () => {
               <h2 className="text-2xl font-bold mb-4">
                 {selectedProject.title}
               </h2>
-              <p className="mb-4">{selectedProject.description}</p>
-              <button
+              <p className="mb-4 text-xl text-center">
+                {selectedProject.description}
+              </p>
+              <motion.button
                 className="px-4 py-2 bg-blue-500 text-white rounded"
                 onClick={() => setSelectedProject(null)}
+                whileTap={{ scale: 0.9 }}
               >
                 Close
-              </button>
+              </motion.button>
               <Link
                 className="px-4 py-2 bg-blue-500 text-white rounded"
                 href={selectedProject.pathName}
