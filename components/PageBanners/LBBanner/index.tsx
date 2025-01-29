b91cc53b192557e27619eb33890353df35232c1b
1medi <144403286+1medi@users.noreply.github.com>
2025-01-28 22:45:06 -0800

    LB PAGE ESTABLISHED

---
+++ components/PageBanners/LBBanner/index.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useScroll,
+  useTransform,
+  animate,
+} from "framer-motion";
+import Image from "next/image";
+
+
+const COLORS = ["#5C47CC", "#8572F4", "#F2E9FF"]; // Shades from the mascot
+const PPBanner = () => {
+  const [isHovered1, setIsHovered1] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const color = useMotionValue(COLORS[0]);
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, ${COLORS[1]} 50%, ${color}`;
+
+  const { scrollY } = useScroll();
+  const opacity = useTransform(scrollY, [0, 300], [1, 0]);
+
+  useEffect(() => {
+    animate(color, COLORS, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
+  return (
+    <>
+      <motion.section
+        key={"banner"}
+        // exit={{ opacity: 0 }}
+        className="about-section min-h-screen relative justify-center shadow-2xl"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          backgroundImage,
+          opacity,
+        }}
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{ duration: 1 }}
+      >
+        <div className="flex flex-col mt-24 justify-center items-center">
+          <div className="flex flex-col">
+              <h1 className="text-center font-bold mb-4 text-7xl flex flex-row">
+                Lutong Bahay
+              </h1>
+            <h2 className="text-3xl font-semibold p-2">
+              Designed and Coded Web App
+            </h2>
+            <h2 className="text-2xl p-2">Completed: December 2024</h2>
+
+            <div className="flex flex-col justify-center items-center">
+              <motion.div
+                onMouseEnter={() => setIsHovered1(true)}
+                onMouseLeave={() => setIsHovered1(false)}
+                animate={{
+                  backgroundColor: isHovered1 ? "#5b21b6" : "",
+                  scale: isHovered1 ? 1.15 : 1,
+                }}
+                transition={{ ease: "backInOut" }}
+                className="bg-violet-900 p-2 rounded-2xl m-4 w-1/2 "
+              >
+                <a href="https://lutong-bahay.vercel.app/" target="_blank">
+                  <p className="text-4xl">Live App</p>
+                </a>
+              </motion.div>
+            </div>
+          </div>
+
+          <motion.div
+            className="m-auto justify-center items-center w-256 h-256"
+            exit={{ opacity: 0 }}
+          ></motion.div>
+        </div>
+      </motion.section>
+    </>
+  );
+};
+
+export default PPBanner;
--- src/app/Lutong-Bahay/page.tsx
+++ src/app/Lutong-Bahay/page.tsx
@@ -1,15 +1,97 @@
 "use client"
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
+import { motion } from "framer-motion";
+import LBBanner from "../../../components/PageBanners/LBBanner";
+import Image from "next/image";
+import PPSitemap from "../../../public/Pocket-Prof/PPSitemap.png";
+import PPPhone from "../../../components/PPPhone";
 
 const LutongBahay = () => {
   return (
     <>
-    <Header/>
-    <main>
-
-    </main>
-    <Footer/>
+<Header />
+      <LBBanner />
+      <main className="min-h-screen flex flex-col items-center justify-center p-4 lg:w-2/3 m-auto">
+        <motion.section className="bg-[#a49e8d] m-4 p-12 rounded-lg">
+          <div className="m-auto justify-center items-center p-8 flex flex-col md:flex-row">
+            <div className="p-4">
+              <h2 className="text-3xl text-center bg-black rounded-lg p-4">
+                Tools Used
+              </h2>
+              <ul className="p-2 text-center">
+                <li className="text-2xl p-2">Figma</li>
+                <li className="text-2xl p-2">Vite</li>
+                <li className="text-2xl p-2">React</li>
+              </ul>
+            </div>
+            <div className="p-4">
+              <h2 className="text-3xl text-center bg-black rounded-lg p-4">
+                Key Features
+              </h2>
+              <ul className="p-2 text-center">
+                <li className="text-2xl p-2">Filipino Recipes</li>
+                <li className="text-2xl p-2">Responsive Design</li>
+                <li className="text-2xl p-2">Skill Library</li>
+              </ul>
+            </div>
+          </div>
+          <div className="about p-8">
+            <h2 className="text-3xl text-center bg-black rounded-lg p-4">
+              What is Lutong Bahay?
+            </h2>
+            <p className="p-4">
+              Pocket Prof is a mobile web application goal tracking app that
+              promotes a variety of skills a user can learn by gathering
+              resources through our ai assistant Oscar. Through its interactive
+              user-friendly features, it provides a platform for students and
+              individuals alike to improve on themselves and create new goals."
+            </p>
+          </div>
+          <div className="proj-ideation p-8">
+            <h2 className="text-3xl text-center md:text-right bg-black rounded-lg p-4">
+              Identifying The Issue
+            </h2>
+            <p className="text-right p-4">
+              There was a gap found in the ability of finding new skills due to
+              the overwhelming surplus of resources available of the internet.
+              Through the app ideation proccess, we determined with the help of
+              AI, we can collect all the available resources and make it
+              available into 1 congestable application: Pocket Prof!
+            </p>
+          </div>
+          <div className="proj-ideation p-8">
+            <h2 className="text-3xl text-center md:text-left bg-black rounded-lg p-4">
+              Design Decisions
+            </h2>
+            <div className="m-auto flex sm:flex-col lg:flex-row">
+              <p className="text-left p-4">
+                This was a rigourous process which involved many steps such as
+                theory crafting, rule making, character design. We were also
+                tasked with determining the flow and gameplay the users
+                experienced.
+              </p>
+            </div>
+            <div className="m-auto flex flex-col">
+              <h2 className="text-center text-4xl font-medium p-4">
+                User Workflow
+              </h2>
+              <Image className="rounded-xl" src={PPSitemap} alt="sitemap" />
+            </div>
+          </div>
+          <div className="proj-outcome p-8">
+            <h2 className="text-3xl bg-black rounded-lg text-center md:text-right p-4">
+              Outcome
+            </h2>
+            <p className="text-center md:text-right p-4">
+              At the conclusion of the project the team produced a playable lofi
+              physical card game, 3 decks (Fire, Water, Ice) with 12 cards each
+              and a menu UI suitable to code for our development team
+            </p>
+          </div>
+        </motion.section>
+      </main>
+      <Footer />
     </>
   )
 }
