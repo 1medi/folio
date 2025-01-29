8cc155516889b1bc46f2289e980afa7710a32ce4
1medi <144403286+1medi@users.noreply.github.com>
2025-01-28 20:18:50 -0800

    was it the animation?

--- components/Animation/Oscar/index.tsx
+++ components/Animation/Oscar/index.tsx
@@ -1,11 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import lottie from 'lottie-web';
 
 export default function Oscar() {
+  const [isBrowser, setIsBrowser] = useState(false); // Track if we're on the client side
   const animationContainer = useRef<HTMLDivElement>(null); // Specify type here
 
   useEffect(() => {
-    if (animationContainer.current) {
+    setIsBrowser(true); // Set to true once we are on the client side
+  }, []);
+
+  useEffect(() => {
+    if (isBrowser && animationContainer.current) {
       lottie.loadAnimation({
         container: animationContainer.current,
         renderer: 'svg',
@@ -14,7 +19,9 @@ export default function Oscar() {
         path: 'banana.json',
       });
     }
-  }, []);
+  }, [isBrowser]); // Ensure it runs only when client-side
+
+  if (!isBrowser) return null; // Prevent rendering on the server side
 
   return (
     <>
