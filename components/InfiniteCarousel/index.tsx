f1cf9c76bd8d05b31c8f71453ef2a13640620b85
1medi <144403286+1medi@users.noreply.github.com>
2025-02-01 13:17:55 -0800

    SHADOW CLONE JITSU

--- components/InfiniteCarousel/index.tsx
+++ components/InfiniteCarousel/index.tsx
@@ -13,43 +13,66 @@ export default function Carousel() {
     { src: "/Pocket-Prof/ppquestion.png", title: "Pocket Prof Question" },
     { src: "/Pocket-Prof/ppquiz.png", title: "Pocket Prof Quiz" },
   ];
-  
-  const links = [
-    "/"
-  ]
+
+  const links = ["/"];
 
   let [ref, { width }] = useMeasure();
   const xTranslation = useMotionValue(0);
 
   useEffect(() => {
-    let finalPosition = -width * 5 -8;
+    let finalPosition = -width * 24 - 8;
     let controls;
 
-      controls = animate(xTranslation, [0, finalPosition], {
-        ease: "linear",
-        duration: 135,
-        repeat: Infinity,
-        repeatType: "loop",
-        repeatDelay: 0,
-      })
+    controls = animate(xTranslation, [0, finalPosition], {
+      ease: "linear",
+      duration: 500,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
 
     return controls?.stop;
-  }, [xTranslation, width,]);
+  }, [xTranslation, width]);
 
   return (
-<div className="overflow-x-hidden py-8 mb-48">
-  <h1 className="text-7xl text-center p-4 font-bold">Other Works</h1>
-  <div className="w-screen overflow-x-hidden">
-    <motion.div
-      className="flex gap-4"
-      ref={ref}
-      style={{ x: xTranslation }}
-    >
-      {[...images, ...images, ...images, ...images,...images,...images].map((item, idx) => (
-        <Card image={item.src} title={item.title} key={idx} />
-      ))}
-    </motion.div>
-  </div>
-</div>
+    <div className="overflow-x-hidden py-8 mb-48">
+      <h1 className="text-7xl text-center p-4 font-bold">Other Works</h1>
+      <div className="w-screen overflow-x-hidden">
+        <motion.div
+          className="flex gap-4"
+          ref={ref}
+          style={{ x: xTranslation }}
+        >
+          {[
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+            ...images,
+          ].map((item, idx) => (
+            <Card image={item.src} title={item.title} key={idx} />
+          ))}
+        </motion.div>
+      </div>
+    </div>
   );
 }
