b6f6411bfb94e7527d0f3fb456b4d8d0720cbc0e
1medi <144403286+1medi@users.noreply.github.com>
2025-02-01 13:04:06 -0800

    HEIGHT FULL YES
    THE ANSWER TO MY PROBLEMS

--- components/PageBanners/Banner/index.tsx
+++ components/PageBanners/Banner/index.tsx
@@ -30,7 +30,7 @@ const Banner = () => {
     });
   }, []);
   return (
-    <main className="w-full relative overflow-hidden">
+    <main className="w-full h-full relative overflow-hidden">
       <motion.section
         className="about-section w-full grid grid-cols-10 h-screen overflow-y-clip"
         style={{
@@ -60,14 +60,14 @@ const Banner = () => {
           animate={{ opacity: 1, y: 1 }}
           transition={{ delay: 1.25 }}
         >
-          <p className="text-2xl m-2 sm:m-auto text-center text-white">
+          <p className="text-2xl m-auto text-center text-white">
             A Front-end developer with a strong passion in modern web
             technologies. With an unwavering interest in user-friendly designs
             and pushing boundaries.
           </p>
         </motion.div>
         <motion.div
-          className="flex justify-center gap-5 m-2 sm:m-auto p-8 pointer-events-auto"
+          className="flex justify-center gap-5 m-auto p-8 pointer-events-auto"
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 1 }}
           transition={{ delay: 1.5 }}
