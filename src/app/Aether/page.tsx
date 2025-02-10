fdd5d37b916052fcafca6acc4287e25554bf9f29
1medi <144403286+1medi@users.noreply.github.com>
2025-02-10 00:38:49 -0800

    no parallax for nao

--- src/app/Aether/page.tsx
+++ src/app/Aether/page.tsx
@@ -20,7 +20,7 @@ import AppComparison from "../../../components/ComparisonSliders/AppComparison";
 const Aether = () => {
   return (
     <>
-      <Parallax />
+      {/* <Parallax /> */}
       <Header />
       <AEBanner />
 
