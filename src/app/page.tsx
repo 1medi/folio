ac6155b3e3e9ca1e7b44ee3df35e673af608be57
1medi <144403286+1medi@users.noreply.github.com>
2025-02-11 05:02:23 -0800

    OK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Banner from "../../components/PageBanners/Banner";
 import Footer from "../../components/Footer";
 import About from "../../components/About";
 import AboutP from "../../components/About-p/index";
-import Lenis from "lenis";
-import { useEffect } from "react";
 import dynamic from "next/dynamic";
 
 const DynamicCarousel = dynamic(() => import('../../components/InfiniteCarousel'), {
@@ -12,14 +10,7 @@ const DynamicCarousel = dynamic(() => import('../../components/InfiniteCarousel'
 })
 
 export default function Home() {
-    useEffect(() => {
-    const lenis = new Lenis();
-    function raf(time: any) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-  }, [])
+
   return (
     <>
       <Banner />
