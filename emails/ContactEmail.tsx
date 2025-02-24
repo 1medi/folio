39c3544784b7df359fa7e16f2b65fe5998351aa9
1medi <144403286+1medi@users.noreply.github.com>
2025-02-24 10:21:26 -0800

    yuh

--- emails/ContactEmail.tsx
+++ emails/ContactEmail.tsx
@@ -38,12 +38,7 @@ export function ContactEmail(props: ContactFormType): ReactElement {
           </Section>
           <Hr style={hr} />
           <Link href="https://www.maditabon.dev">
-            <Img
-              src="https://imgur.com/a/66znC9m"
-              alt="Madi Tabon"
-              width="250"
-              style={logo}
-            />
+          Madi Tabon's Portfolio
           </Link>
         </Container>
       </Body>
